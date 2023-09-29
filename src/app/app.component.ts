956abecebdec3d2c698e0684cab23836e85c1689
Kai Salmen <kai.salmen@typefox.io>
2023-09-29 14:19:19 +0200

    Update to monaco-languageclient 6.5.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,8 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import 'monaco-editor/esm/vs/editor/editor.all.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/accessibilityHelp/accessibilityHelp.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/iPadShowKeyboard/iPadShowKeyboard.js';
-import { languages, Uri } from 'monaco-editor/esm/vs/editor/editor.api.js';
-import { createConfiguredEditor, createModelReference } from 'vscode/monaco';
-import { initServices, MonacoLanguageClient } from 'monaco-languageclient';
-import { toSocket, WebSocketMessageReader, WebSocketMessageWriter } from 'vscode-ws-jsonrpc';
-import normalizeUrl from 'normalize-url';
+import { createDefaultJsonContent, createJsonEditor, createUrl, createWebSocketAndStartClient, performInit } from 'monaco-languageclient-examples';
 import { AfterViewInit, Component } from '@angular/core';
-import { CloseAction, ErrorAction, MessageTransports } from 'vscode-languageclient/lib/common/client.js';
-import 'vscode/default-extensions/theme-defaults';
-import 'vscode/default-extensions/json';
-
 import { buildWorkerDefinition } from 'monaco-editor-workers';
 buildWorkerDefinition('./assets/monaco-editor-workers/workers', window.location.href + '../..', false);
 
@@ -28,101 +17,17 @@ export class MonacoEditorComponent implements AfterViewInit {
     title = 'angular-client';
     initDone = false;
 
-    createLanguageClient(transports: MessageTransports): MonacoLanguageClient {
-        return new MonacoLanguageClient({
-            name: 'Sample Language Client',
-            clientOptions: {
-                // use a language id as a document selector
-                documentSelector: ['json'],
-                // disable the default error handler
-                errorHandler: {
-                    error: () => ({ action: ErrorAction.Continue }),
-                    closed: () => ({ action: CloseAction.DoNotRestart })
-                }
-            },
-            // create a language client connection from the JSON RPC connection on demand
-            connectionProvider: {
-                get: () => {
-                    return Promise.resolve(transports);
-                }
-            }
-        });
-    }
-
-    createUrl(hostname: string, port: number, path: string): string {
-        const protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-        return normalizeUrl(`${protocol}://${hostname}:${port}${path}`);
-    }
-
-    createWebSocket(url: string): WebSocket {
-        const webSocket = new WebSocket(url);
-        webSocket.onopen = () => {
-            const socket = toSocket(webSocket);
-            const reader = new WebSocketMessageReader(socket);
-            const writer = new WebSocketMessageWriter(socket);
-            const languageClient = this.createLanguageClient({
-                reader,
-                writer
-            });
-            languageClient.start();
-            reader.onClose(() => languageClient.stop());
-        };
-        return webSocket;
-    }
-
-    private createDefaultJsonContent(): string {
-        return `{
-    "$schema": "http://json.schemastore.org/coffeelint",
-    "line_endings": "unix"
-}`;
-    }
-
     async ngAfterViewInit(): Promise<void> {
-        const languageId = 'json';
-
-        if (!this.initDone) {
-            await initServices({
-                enableFilesService: true,
-                enableKeybindingsService: true,
-                enableQuickaccessService: true,
-                enableThemeService: true,
-                enableTextmateService: true,
-                enableModelService: true,
-                configureEditorOrViewsServiceConfig: {
-                    enableViewsService: false,
-                    useDefaultOpenEditorFunction: true
-                },
-                enableLanguagesService: true,
-                debugLogging: true
-            });
-            this.initDone = true;
-        }
-
-        // register the JSON language with Monaco
-        languages.register({
-            id: languageId,
-            extensions: ['.json', '.jsonc'],
-            aliases: ['JSON', 'json'],
-            mimetypes: ['application/json']
-        });
-
-        // create the model
-        const uri = Uri.parse('/tmp/model.json');
-        const modelRef = await createModelReference(uri, this.createDefaultJsonContent());
-        modelRef.object.setLanguageId(languageId);
-
-        // create monaco editor
-        createConfiguredEditor(document.getElementById('container')!, {
-            model: modelRef.object.textEditorModel,
-            glyphMargin: true,
-            lightbulb: {
-                enabled: true
-            },
-            automaticLayout: true
+        // use the same common method to create a monaco editor for json
+        await performInit(!this.initDone);
+        this.initDone = true;
+        await createJsonEditor({
+            htmlElement: document.getElementById('container')!,
+            content: createDefaultJsonContent()
         });
 
         // create the web socket
-        const url = this.createUrl('localhost', 3000, '/sampleServer');
-        this.createWebSocket(url);
+        const url = createUrl('localhost', 3000, '/sampleServer');
+        createWebSocketAndStartClient(url);
     }
 }
